31a79bc918835a65e27a23b62c46961459f4a987
ak1t0 <aktoo3097@gmail.com>
2017-08-01 22:38:29 +0900

    add cli

--- main.go
+++ main.go
@@ -1,15 +1,58 @@
 package main
 
 import (
+	"github.com/urfave/cli"
 	"github.com/ak1t0/flame/crawler"
 	"github.com/ak1t0/flame/format"
 	"github.com/ak1t0/flame/reader"
 	"log"
+	"os"
 )
 
-func main() {
-	parsed := reader.ReadJson("flame.json")
-	r := crawler.Scan(format.NewOnionLogs(parsed))
+var Version string = "0.0.1"
 
-	log.Println(r)
+func main() {
+	app := cli.NewApp()
+	app.Name = "flame"
+	app.Usage = "crawl onion services"
+	app.Version = Version
+	app.Author = "ak1t0"
+	app.Email = "aktoo3097@gmail.com"
+	app.Commands = Commands 
+
+	app.Run(os.Args)
 }
+
+var Commands = []cli.Command{
+	commandScan,
+}
+
+var commandScan = cli.Command{
+	Name: "scan",
+	Usage: "Scan onion services",
+	Aliases: []string{"s"}, 
+	Action: doScan,
+	Flags: []cli.Flag {
+		cli.StringFlag{
+			Name: "f",
+			Usage: "Select log file",
+		},
+	},
+}
+
+func doScan(c *cli.Context) error {
+	var target string
+	if c.String("f") != "" {
+		target = c.String("f")
+	} else {
+		target = "log.json"
+	}
+
+	parsed := reader.ReadJson(target)
+	r := crawler.Scan(format.NewOnionLogs(parsed))
+	
+	log.Println(r)
+		
+	return nil
+}
+
