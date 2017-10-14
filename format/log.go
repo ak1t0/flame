510cc31766b241645ef47b3d2a6fdca1aed40e44
ak1t0 <aktoo3097@gmail.com>
2017-10-14 17:07:31 +0900

    add access counter to format constructor

--- format/log.go
+++ format/log.go
@@ -17,5 +17,32 @@ func NewOnionLogs(log []string) OnionLogs {
 		l = append(l, OnionLog{Address: v})
 	}
 
+	l = CountOnionLogs(l)
+
+	return l
+}
+
+func CountOnionLogs(logs OnionLogs) OnionLogs {
+	var l OnionLogs
+	counter := make(map[string]int)
+
+	for _, v := range logs {
+		if val, ok := counter[v.Address]; ok {
+			counter[v.Address] = val + 1
+		} else {
+			counter[v.Address] = 1
+		}
+	}
+
+	keys := make([]string, 0, len(counter))
+
+	for k := range counter {
+		keys = append(keys, k)
+	}
+
+	for _, k := range keys {
+		l = append(l, OnionLog{Address: k, Access: counter[k]})
+	}
+
 	return l
 }
