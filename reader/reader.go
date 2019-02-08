e9b05696a683bb5663cd31640954764b9158ac22
ak1t0 <aktoo3097@gmail.com>
2019-02-09 00:11:03 +0900

    add text to input option

--- reader/reader.go
+++ reader/reader.go
@@ -1,13 +1,27 @@
 package reader
 
 import (
+	"bufio"
 	"encoding/json"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
 type Parsed struct {
 	Domain string `json:"domain"`
+}
+
+func ReadData(name string) ([]string, error) {
+	if strings.Contains(name, ".json") {
+		return ReadJson(name)
+	}
+	if strings.Contains(name, ".txt") {
+		return ReadText(name)
+	} else {
+		var s []string
+		return s, nil
+	}
 }
 
 func ReadJson(name string) ([]string, error) {
@@ -30,6 +44,23 @@ func ReadJson(name string) ([]string, error) {
 	var domains []string
 	for _, log := range logs {
 		domains = append(domains, log.Domain)
+	}
+
+	return domains, nil
+}
+
+func ReadText(name string) ([]string, error) {
+	fp, err := os.Open(name)
+	if err != nil {
+		return nil, err
+	}
+	defer fp.Close()
+
+	scanner := bufio.NewScanner(fp)
+
+	var domains []string
+	for scanner.Scan() {
+		domains = append(domains, scanner.Text())
 	}
 
 	return domains, nil
